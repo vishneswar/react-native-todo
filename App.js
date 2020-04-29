90ce1d0c83dd9fc915982c647eac81cf8c4e3b22
vishneswar <58622167+vishneswar@users.noreply.github.com>
2020-04-29 15:49:35 +0530

    Added the homepage style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View , Button, ScrollView} from 'react-native';
+import { Platform, StyleSheet, Text, View , Button, ScrollView, TouchableOpacity} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 const instructions = Platform.select({
@@ -7,69 +7,157 @@ const instructions = Platform.select({
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>To Do application</Text>
-      <ScrollView>
-        <View style={styles.txttxt}><Text style={styles.txt}>Task 1</Text></View>
-        <Text style={styles.txt}>Task 1</Text>
-        <Text style={styles.txt}>Task 1</Text>
-        <Text style={styles.txt}>Task 1</Text>
-        <Text style={styles.txt}>Task 1</Text>
-      </ScrollView>
-      <View style={styles.input}>
-      <TextInput />
-      </View>
-      
-      <View style={styles.btnView}>
-        <Button style={styles.btn} title="Add task"/>
-      </View>
-    </View>
-    
-  );
-}
+export default class App extends React.Component {
 
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>tasker</Text>
+        <ScrollView>
+          <View style={styles.detailsView}>
+            <Text style={styles.h1}>12</Text>
+            <Text style={styles.h2}>Tasks for today</Text>
+            <Text style={styles.h3}>2 tasks done</Text>
+          </View>
+
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+          <View><Text style={styles.tasklist}>Buy Milk</Text> 
+            <TouchableOpacity style={styles.completeButton}>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.addBtn}>+</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+//alignItems: 'center',
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
     marginTop: 26,
   },
   welcome: {
     fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
+    color: '#EA7773',
     marginTop: 20,
+    paddingLeft: 20,
     paddingBottom:10,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
     width: 370,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  detailsView: {
+    padding: 20,
+    paddingBottom: 30,
   },
-  btn: {
-    marginBottom: 10,
+  h1: {
+    fontSize: 90,
+    color: '#EA7773',
+    fontWeight: 'bold'
   },
-  input: {
-    borderWidth: 3,
-    borderColor: '#e7e7e7',
-    width: 380,
-    height: 50,
-    marginBottom: 10,
-    borderRadius: 10,
-    elevation: 10,
+  h2: {
+    fontSize: 40,
+    color: '#0A79DF',
+    fontWeight: 'bold',
   },
-  btnView: {
-    paddingBottom: 10,
-  },
-  txt: {
+  h3: {
     fontSize: 20,
-    paddingBottom: 10,
+    color: '#8395A7',
+    paddingLeft: 10,
   },
+  tasklist: {
+    fontSize: 20,
+    paddingLeft: 50,
+    paddingRight: 20,
+    paddingTop: 2,
+    marginBottom: 20,
+    color: '#758AA2',
+    borderBottomWidth: 1,
+    borderColor: '#DAE0E2',
+    paddingBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    fontWeight: '900',  
+  },
+  completeButton: {
+    borderColor: '#777E8B',
+    borderWidth: 1,
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    justifyContent: 'flex-end',
+    position: 'absolute',
+    marginLeft: 25,
+  },
+  addButton: {
+    justifyContent: 'flex-end',
+    position: 'absolute',
+    bottom: 0,
+    padding: 5,
+    paddingRight: 25,
+    paddingBottom: 15,
+    alignSelf: 'flex-end',
+  },
+  addBtn : {
+    textAlign: 'center',
+    fontSize: 30,
+    borderWidth: 1,
+    width: 70,
+    height: 70,
+    borderRadius: 70,
+    paddingTop: 15,
+    borderColor: '#EA7773',
+    color: '#EA7773',
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+  },
+  
 });
